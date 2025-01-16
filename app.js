9500a9f843d7ee0ac4b71f8e4883f0b8c4a2e120
mo1726 <mlahrech123@gmail.com>
2025-01-16 22:51:13 +0100

    v1

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ const mongoose = require("mongoose");
 app.use(express.urlencoded({extended: true}));
 const Mydata=require("./models/SCHEMA");
 
-
+//get method for get the page route 
 app.get("/", (req, res) => {
   res.sendFile("/views/home.html", { root: __dirname });
 });
 
+//post method for add data in mongodb collection route
 app.post("/" ,(req,res)=>{
   const mydata=new Mydata({
     name:req.body.name
@@ -24,7 +25,7 @@ app.post("/" ,(req,res)=>{
 
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/Myproject")
+  .connect("mongodb://127.0.0.1:27017/Myproject") //MongoDb connection link
   .then(() => {
     app.listen(port, () => {
       console.log(`Example app listening on port ${port}`);
